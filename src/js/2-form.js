733ab8473d1a2773a9c57907dc477646bbdd4990
vitalii <vitalii.cherukha@gmail.com>
2025-07-11 16:12:13 +0300

    add files

---
+++ src/js/2-form.js
@@ -0,0 +1,55 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const fillFeedbackForm = feedbackForm => {
+  try {
+    const feedbackFormLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (feedbackFormLS === null) {
+      return;
+    }
+
+    formData = feedbackFormLS;
+
+    const feedbackFormLSKeys = Object.keys(feedbackFormLS);
+
+    feedbackFormLSKeys.forEach(key => {
+      refs.form.elements[key].value = feedbackFormLS[key];
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fillFeedbackForm(formData);
+
+const onFormInput = evn => {
+  const { email, message } = evn.currentTarget;
+  formData.email = email.value.trim();
+  formData.message = message.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFormSubmit = evn => {
+  evn.preventDefault();
+  if (
+    evn.currentTarget.email.value === '' ||
+    evn.currentTarget.message.value === ''
+  ) {
+    alert('Fill please all fields');
+    return;
+  }
+  evn.target.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('submit', onFormSubmit);
